e17a0458a45d520618cd06bc5614283a3a5ecb4b
Volumio Mytek Build <volumio@localhost>
2026-09-12 20:18:23 +0200

    Fix firmware re-probe after USB re-enumeration

--- chip.c
+++ chip.c
@@ -111,10 +111,16 @@ static int mytek_chip_probe(struct usb_interface *intf,
 	/* check, if firmware is present on device, upload it if not */
 	ret = mytek_fw_init(intf);
 
-	if (ret < 0)
-		return ret;
-	else if (ret == FW_NOT_READY) /* firmware update performed */
-		return 0;
+	if (ret < 0 || ret == FW_NOT_READY) {
+		mutex_lock(&register_mutex);
+		if (devices[regidx] == device && !chips[regidx])
+			devices[regidx] = NULL;
+		mutex_unlock(&register_mutex);
+
+		if (ret < 0)
+			return ret;
+		return 0; /* firmware update performed */
+	}
 
 	/* if we are here, card can be registered in alsa. */
 	if (usb_set_interface(device, 0, 0) != 0) {
